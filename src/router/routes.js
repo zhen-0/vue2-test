7aa2105fbc7641191c37e0b21fa4296c57efe4c7
zhen-0 <1277995737@qq.com>
2022-06-21 23:37:23 +0800

    初始化路由

---
+++ src/router/routes.js
@@ -0,0 +1,15 @@
+// 添加路由路径
+const format = () => import('../views/format.vue')
+
+export default [
+  {
+    path: '/',
+    name: 'Home',
+    redirect: '/format'
+  },
+  {
+    path: '/format',
+    name: 'Format',
+    component: format
+  }
+]
---
+++ src/views/format.vue
@@ -0,0 +1,16 @@
+<template>
+<!--eslint&prrient 格式化 -->
+    <div>
+eslint&prrient 格式化
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
